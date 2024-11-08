13f0ca014d8478d61f697e20c8b8068b30b8f03f
Lui Zappitelli <luizappitelli@gmail.com>
2024-11-08 15:24:21 -0600

    joe test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette={"red"}>Click me</Button>;
+  const large = window.innerWidth > 768;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <Show when={large}>
+        <GridItem area={"nav"} bg={"coral"}>
+          Nav
+        </GridItem>
+      </Show>
+      <GridItem
+        area={"aside"}
+        bg={"gold"}
+        display={{ base: "none", lg: "block" }}
+      >
+        Aside
+      </GridItem>
+
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
